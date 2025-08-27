deec503edcc1658607ba73dae284708b2ca24bb1
Juan Laspiur <laspiurperezjuan@gmail.com>
2025-08-27 19:41:09 -0300

    Guardar

--- src/presentation/auth/controller.ts
+++ src/presentation/auth/controller.ts
@@ -1,9 +1,18 @@
 import { Request, Response } from "express";
 import { RegisterDto, LoginDto } from "../../domain/dtos";
-import { AuthRepository } from "../../domain";
+import { AuthRepository, CustomError } from "../../domain";
 
 export class AuthController {
 constructor(private readonly authRepository: AuthRepository) {}
+
+private handleError = (error: unknown, res: Response) => {
+  if (error instanceof CustomError) {
+    return res.status(error.statusCode).json({ error: error.message });
+  }
+  console.error(error);
+  return res.status(500).json({ error: 'Internal server error' });
+}
+
 
 
     register(req: Request, res: Response) {
@@ -13,7 +22,7 @@ constructor(private readonly authRepository: AuthRepository) {}
         }
          this.authRepository.register(registerDto!)
          .then(user=> res.status(200).json(user))
-         .catch(error => res.status(500).json(error))
+         .catch(error => this.handleError(error, res))
         
         
        
@@ -26,7 +35,7 @@ constructor(private readonly authRepository: AuthRepository) {}
         }
        this.authRepository.login(loginDto!)
        .then(user=> res.status(200).json(user))
-       .catch(error => res.status(500).json(error));
+       .catch(error => this.handleError(error, res));
     }
 
 }

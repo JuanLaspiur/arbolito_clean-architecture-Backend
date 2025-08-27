dda793dbba9eb42dc8d45eb53ee8365840b371b9
Juan Laspiur <laspiurperezjuan@gmail.com>
2025-08-27 17:41:25 -0300

    Guardar

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,18 @@
-import "dotenv/config"; 
+import "dotenv/config";
 import { Server } from "./presentation/server";
 import { AppRoutes } from "./presentation/routes";
+import { MongoDataBase } from "./data/mogodb";
 
 (() => {
   main();
 })();
 
-function main() {
+async function main() {
+  await MongoDataBase.connect({
+    dbName: process.env.MONGO_DB_NAME!,
+    mongoUrl: process.env.MONGO_URL!,
+  });
+
   const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
   const server = new Server(port, AppRoutes.routes);
 

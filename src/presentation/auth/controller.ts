2ecbf8e83fa2aff77f12b40ba5026ab670060026
Juan Laspiur <laspiurperezjuan@gmail.com>
2025-08-27 16:45:39 -0300

    sace

--- src/presentation/auth/controller.ts
+++ src/presentation/auth/controller.ts
@@ -1,23 +1,29 @@
 import { Request, Response } from "express";
 import { RegisterDto, LoginDto } from "../../domain/dtos";
+import { AuthRepository } from "../../domain";
 
 export class AuthController {
-    constructor() { }
+    constructor(private readonly authRepositoy : AuthRepository) { }
 
     register(req: Request, res: Response) {
         const [error, registerDto] = RegisterDto.create(req.body);
         if (error) {
             return res.status(400).json({ error });
         }
-        return res.status(200).json(registerDto);
+        this.authRepositoy.register(registerDto!)
+        .then(user=> res.status(200).json(user))
+        .catch(error => res.status(500).json(error));
+        ;
     }
 
     login(req: Request, res: Response) {
-              const [error, loginDto] = LoginDto.create(req.body);
+        const [error, loginDto] = LoginDto.create(req.body);
         if (error) {
             return res.status(400).json({ error });
         }
-        return res.status(200).json(loginDto);
+       this.authRepositoy.login(loginDto!)
+       .then(user=> res.status(200).json(user))
+       .catch(error => res.status(500).json(error));
     }
 
 }
--- src/presentation/auth/routes.ts
+++ src/presentation/auth/routes.ts
@@ -1,9 +1,15 @@
 import { Router } from "express";
 import { AuthController } from "./controller";
+import { AuthDataSourceImpl, AuthRepositoryImpl } from "../../infrastructure";
 
 export class AuthRoutes {
     static get routes():Router {
-    const controller = new AuthController();
+    
+    const datasource = new AuthDataSourceImpl();    
+    const authRepositoy = new AuthRepositoryImpl(datasource);    
+
+    const controller = new AuthController(authRepositoy);
+
     const router = Router();
     router.post('/register',controller.register);
     router.post('/login',controller.login);

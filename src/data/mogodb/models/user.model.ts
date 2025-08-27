d9d096151e90d9c44258ac9222d016a2b4446d0e
Juan Laspiur <laspiurperezjuan@gmail.com>
2025-08-27 17:55:20 -0300

    sve

---
+++ src/data/mogodb/models/user.model.ts
@@ -0,0 +1,33 @@
+import mongoose, { Schema } from 'mongoose';
+
+const UserSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Name is required'],
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, 'Password is required'],
+    },
+    role: {
+      type: String,
+      enum: ['admin', 'user'],
+      default: 'user',
+    },
+  },
+  {
+    timestamps: true, 
+    versionKey: false,
+  }
+);
+
+export const UserModel = mongoose.model('User', UserSchema);
